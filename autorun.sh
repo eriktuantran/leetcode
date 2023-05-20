ceac8477de4ff6a6610d347a245d0a510d5af30d
Tuan Tran <quoctuanuit@gmail.com>
2023-05-20 16:55:12 +0200

    cleanup cov

--- autorun.sh
+++ autorun.sh
@@ -1,4 +1,5 @@
 make
 ./runUnitTests
 lcov --directory . --capture --output-file coverage.info
+lcov --remove coverage.info 'tests/*' '/usr/*' 'test-library*' '*modules/googletest*' '*MacOS*' --output-file coverage.info
 lcov --list coverage.info
