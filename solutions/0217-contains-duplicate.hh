cb8fe3078c58cd91d95ac7948f15fadb116cd889
Tuan Tran <quoctuanuit@gmail.com>
2023-06-24 15:03:55 +0200

    two sum

--- solutions/0217-contains-duplicate.hh
+++ solutions/0217-contains-duplicate.hh
@@ -2,13 +2,15 @@
 #include <unordered_set>
 #include <vector>
 
-using namespace std;  // I dont like this, but it's a leetcode thing
-class Solution {
- public:
-  bool containsDuplicate(std::vector<int> &nums);
-  bool containsNearbyDuplicate(std::vector<int> &nums, int k);
-  bool containsNearbyAlmostDuplicate(std::vector<int> &nums, int indexDiff, int valueDiff);
-  bool isAnagram(string s, string t);
+using namespace std; // I dont like this, but it's a leetcode thing
+class Solution
+{
+  public:
+    bool containsDuplicate(std::vector<int> &nums);
+    bool containsNearbyDuplicate(std::vector<int> &nums, int k);
+    bool containsNearbyAlmostDuplicate(std::vector<int> &nums, int indexDiff, int valueDiff);
+    bool isAnagram(string s, string t);
+    vector<int> twoSum(vector<int> &nums, int target);
 };
 
 void independentMethod(int &i);
