51fd0e1033b13f13052c3144b3467c92bf694c77
Tuan Tran <quoctuanuit@gmail.com>
2023-06-24 16:14:20 +0200

    isPalindrome

--- solutions/two-pointers.cc
+++ solutions/two-pointers.cc
@@ -13,7 +13,21 @@ using namespace std; // I dont like this, but it's a leetcode thing
 // https://leetcode.com/problems/valid-palindrome/
 // Input: s = "A man, a plan, a canal: Panama"
 // Output: true
-// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Explanation: "amanaplana c analpanama" is a palindrome.
 bool Solution::isPalindrome(string s) {
+    int i = 0, j = s.size() - 1;
+    while (i < j) {
+        while (!isalnum(s[i]) && i < j) {
+            i++;
+        }
+        while (!isalnum(s[j]) && j > i) {
+            j--;
+        }
+        if (tolower(s[i]) != tolower(s[j])) {
+            return false;
+        }
+        i++;
+        j--;
+    }
     return true;
 }
--- test/testcases.cc
+++ test/testcases.cc
@@ -131,7 +131,7 @@ TEST_F(TestSuite, isPalindrome) {
     EXPECT_EQ(true, s.isPalindrome("A man, a plan, a canal: Panama"));
     EXPECT_EQ(false, s.isPalindrome("race a car"));
     EXPECT_EQ(true, s.isPalindrome(" "));
-    // EXPECT_EQ(true, s.isAnagram(isAnagram_data.first, isAnagram_data.second));
+    EXPECT_EQ(true, s.isPalindrome(isPalindrome_data));
 }
 
 // }  // namespace - could surround Project1Test in a namespace
