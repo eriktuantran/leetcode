69cf602bd921efd9ad57720f6c89b999a3224dc0
Tuan Tran <quoctuanuit@gmail.com>
2023-06-25 14:33:15 +0200

    restructure test

--- test/array-and-hashing-tescases.cc
+++ test/array-and-hashing-tescases.cc
@@ -1,56 +1,5 @@
-#include <iostream>
-
-#include "solutions.hh"
-#include "gtest/gtest.h"
-#include "test-data.hh"
-
-// IndependentMethod is a test case - here, we have 2 tests for this 1 test case
-TEST(IndependentMethod, ResetsToZero) {
-    int i = 3;
-    independentMethod(i);
-    EXPECT_EQ(0, i);
-
-    i = 12;
-    independentMethod(i);
-    EXPECT_EQ(0, i);
-}
-
-TEST(IndependentMethod, ResetsToZero2) {
-    int i = 0;
-    independentMethod(i);
-    EXPECT_EQ(0, i);
-}
-
-// The fixture for testing class Project1. From google test primer.
-class TestSuite : public ::testing::Test
-{
-  protected:
-    // You can remove any or all of the following functions if its body
-    // is empty.
-
-    TestSuite() {
-        // You can do set-up work for each test here.
-    }
-
-    virtual ~TestSuite() {
-        // You can do clean-up work that doesn't throw exceptions here.
-    }
-
-    // If the constructor and destructor are not enough for setting up
-    // and cleaning up each test, you can define the following methods:
-    virtual void SetUp() {
-        // Code here will be called immediately after the constructor (right
-        // before each test).
-    }
-
-    virtual void TearDown() {
-        // Code here will be called immediately after each test (right
-        // before the destructor).
-    }
-
-    // Objects declared here can be used by all tests in the test case for Project1.
-    Solution s;
-};
+#include "testsuite.hh"
+#include "array-and-hashing-data.hh"
 
 // Test case must be called the class above
 // Also note: use TEST_F instead of TEST to access the test fixture (from google test primer)
@@ -133,13 +82,3 @@ TEST_F(TestSuite, isPalindrome) {
     EXPECT_EQ(true, s.isPalindrome(" "));
     EXPECT_EQ(true, s.isPalindrome(isPalindrome_data));
 }
-
-TEST_F(TestSuite, maxProfit) {
-    std::vector<int> data = { 7, 1, 5, 3, 6, 4 };
-    EXPECT_EQ(5, s.maxProfit(data));
-    data = { 7, 6, 4, 3, 1 };
-    EXPECT_EQ(0, s.maxProfit(data));
-    EXPECT_EQ(999, s.maxProfit(maxProfit_data));
-}
-
-// }  // namespace - could surround Project1Test in a namespace
